5d7cf6277b2e1a39143df011f6fb1d6aef6d398c
Alexey Anisimov <alexey@spamexperts.com>
2015-08-17 18:39:39 +0000

    Refs. #24721. More tests were added.

--- tests/unit/test_plugins/test_whitelist_subject.py
+++ tests/unit/test_plugins/test_whitelist_subject.py
@@ -36,6 +36,7 @@ class TestWhitelistSubject(unittest.TestCase):
         self.global_data = {}
         self.msg_data = {}
         patch("sa.plugins.whitelist_subject.WhiteListSubjectPlugin.options", self.options).start()
+        patch("sa.plugins.whitelist_subject.WhiteListSubjectPlugin.inhibit_further_callbacks").start()
 
         self.mock_ctxt = MagicMock(**{
             "get_plugin_data.side_effect": lambda p, k: self.global_data[k],
@@ -55,21 +56,28 @@ class TestWhitelistSubject(unittest.TestCase):
         self.msg = email.message_from_string(MESSAGE.format("Test subject"))
         self.mock_msg.msg = self.msg
         plugin = sa.plugins.whitelist_subject.WhiteListSubjectPlugin(self.mock_ctxt)
-        self.assertTrue(plugin.check_subject_in_whitelist(self.mock_msg))
+        self.assertTrue(plugin._check_subject(self.mock_msg, self.options["whitelist_subject"][1]))
 
     def test_check_subject_one_regex_false(self):
         self.options["whitelist_subject"] = ("list", [r"[a-z]+"])
         self.msg = email.message_from_string(MESSAGE.format("Test subject"))
         self.mock_msg.msg = self.msg
         plugin = sa.plugins.whitelist_subject.WhiteListSubjectPlugin(self.mock_ctxt)
-        self.assertFalse(plugin.check_subject_in_whitelist(self.mock_msg))
+        self.assertFalse(plugin._check_subject(self.mock_msg, self.options["whitelist_subject"][1]))
 
     def test_check_subject_one_regex_blank(self):
         self.options["whitelist_subject"] = ("list", [])
         self.msg = email.message_from_string(MESSAGE.format("Test subject"))
         self.mock_msg.msg = self.msg
         plugin = sa.plugins.whitelist_subject.WhiteListSubjectPlugin(self.mock_ctxt)
-        self.assertFalse(plugin.check_subject_in_whitelist(self.mock_msg))
+        self.assertFalse(plugin._check_subject(self.mock_msg, self.options["whitelist_subject"][1]))
+
+    def test_check_subject_list_regex(self):
+        self.options["whitelist_subject"] = ("list", [r"[a-z]+", r".*\d$", r"^\d.*"])
+        self.msg = email.message_from_string(MESSAGE.format("1Test subject"))
+        self.mock_msg.msg = self.msg
+        plugin = sa.plugins.whitelist_subject.WhiteListSubjectPlugin(self.mock_ctxt)
+        self.assertTrue(plugin._check_subject(self.mock_msg, self.options["whitelist_subject"][1]))
 
     def test_check_subject_one_regex_black(self):
         self.options["blacklist_subject"] = ("list", [r"[a-zA-Z]+"])
@@ -78,6 +86,47 @@ class TestWhitelistSubject(unittest.TestCase):
         self.mock_msg.msg = self.msg
         plugin = sa.plugins.whitelist_subject.WhiteListSubjectPlugin(self.mock_ctxt)
         self.assertFalse(plugin.check_subject_in_whitelist(self.mock_msg))
+        self.assertTrue(plugin.check_subject_in_blacklist(self.mock_msg))
+
+    def test_check_subject_one_regex_white(self):
+        self.options["whitelist_subject"] = ("list", [r"[a-zA-Z]+"])
+        self.options["blacklist_subject"] = ("list", [])
+        self.msg = email.message_from_string(MESSAGE.format("Test subject"))
+        self.mock_msg.msg = self.msg
+        plugin = sa.plugins.whitelist_subject.WhiteListSubjectPlugin(self.mock_ctxt)
+        self.assertFalse(plugin.check_subject_in_blacklist(self.mock_msg))
+        self.assertTrue(plugin.check_subject_in_whitelist(self.mock_msg))
+
+    def test_set_append_option(self):
+        self.options["whitelist_subject"] = ("list", [r"[a-zA-Z]+"])
+        new_option = r"^\d.*"
+        plugin = sa.plugins.whitelist_subject.WhiteListSubjectPlugin(self.mock_ctxt)
+        plugin.set_append_option("whitelist_subject", new_option)
+        self.assertEqual(self.options["whitelist_subject"], ('list', ['[a-zA-Z]+', new_option]))
+
+    def test_parse_config(self):
+        self.options["whitelist_subject"] = ("list", [r"[a-zA-Z]+"])
+        self.options["blacklist_subject"] = ("list", [])
+        new_option = r"^\d.*"
+        plugin = sa.plugins.whitelist_subject.WhiteListSubjectPlugin(self.mock_ctxt)
+        plugin.parse_config("whitelist_subject", new_option)
+        self.assertEqual(self.options["whitelist_subject"], ('list', ['[a-zA-Z]+', new_option]))
+
+    def test_parse_config_bad_regex(self):
+        self.options["whitelist_subject"] = ("list", [r"[a-zA-Z]+"])
+        self.options["blacklist_subject"] = ("list", [])
+        new_option = "he(lo"
+        plugin = sa.plugins.whitelist_subject.WhiteListSubjectPlugin(self.mock_ctxt)
+        plugin.parse_config("whitelist_subject", new_option)
+        self.assertEqual(self.options["whitelist_subject"], ('list', ['[a-zA-Z]+']))
+
+    def test_parse_config_wrong_key(self):
+        self.options["whitelist_subject"] = ("list", [r"[a-zA-Z]+"])
+        self.options["blacklist_subject"] = ("list", [])
+        new_option = r"^\d.*"
+        plugin = sa.plugins.whitelist_subject.WhiteListSubjectPlugin(self.mock_ctxt)
+        plugin.parse_config("whitelist", new_option)
+        self.assertEqual(self.options["whitelist_subject"], ('list', ['[a-zA-Z]+']))
 
 
 def suite():
