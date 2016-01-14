c0287056f9e59dccdda02b4f5e17358409a756f6
Alexey Anisimov <aragorna@gmail.com>
2016-01-14 08:19:36 +0300

    Add parse_pad_rules exceptions tests for match.py script

--- tests/unit/test_match.py
+++ tests/unit/test_match.py
@@ -10,6 +10,7 @@ except ImportError:
     from mock import patch, Mock, MagicMock, mock_open, call
 
 import scripts.match
+import pad.errors
 
 EXAMPLE_EMAIL = ()
 
@@ -53,10 +54,36 @@ class TestRevokeReport(unittest.TestCase):
         self.context.hook_revoke.assert_has_calls(calls)
         self.context.hook_report.assert_not_called()
 
+    def test_max_recursion_exception(self):
+        options = scripts.match.parse_arguments(["--revoke",
+                                                 "--siteconfigpath", ".",
+                                                 "--configpath", "."])
+        options.messages = [[StringIO(x) for x in self.messages]]
+        patch("scripts.match.parse_arguments",
+              return_value=options).start()
+        self.mock_parse = patch(
+            "pad.rules.parser.parse_pad_rules",
+            side_effect=pad.errors.MaxRecursionDepthExceeded).start()
+        with self.assertRaises(SystemExit):
+            scripts.match.main()
+
+    def test_parsing_error_exception(self):
+        options = scripts.match.parse_arguments(["--revoke",
+                                                 "--siteconfigpath", ".",
+                                                 "--configpath", "."])
+        options.messages = [[StringIO(x) for x in self.messages]]
+        patch("scripts.match.parse_arguments",
+              return_value=options).start()
+        self.mock_parse = patch(
+            "pad.rules.parser.parse_pad_rules",
+            side_effect=pad.errors.ParsingError).start()
+        with self.assertRaises(SystemExit):
+            scripts.match.main()
+
     def test_both(self):
         with self.assertRaises(SystemExit):
             with patch("sys.stderr"):
-                options = scripts.match.parse_arguments(["--revoke", "--report", "-"])
+                scripts.match.parse_arguments(["--revoke", "--report", "-"])
 
 
 
