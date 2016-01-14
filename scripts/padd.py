fc1d21a6ce9c11e0483d7213a6c6c8777de68865
Alexandru Chirila <alex@alexkiro.com>
2016-01-14 15:37:15 +0200

    Adjust the setup script to deploy the daemon.

--- scripts/padd.py
+++ scripts/padd.py
@@ -2,6 +2,7 @@
 
 """Starts the SpamPAD daemon."""
 
+from __future__ import print_function
 from __future__ import absolute_import
 
 import os
@@ -11,6 +12,62 @@ import argparse
 import pad
 import pad.config
 import pad.server
+
+
+def detach(stdout="/dev/null", stderr=None, stdin="/dev/null", pidfile=None):
+    """This forks the current process into a daemon.
+
+    The stdin, stdout, and stderr arguments are file names that
+    will be opened and be used to replace the standard file descriptors
+    in sys.stdin, sys.stdout, and sys.stderr.
+
+    These arguments are optional and default to /dev/null.
+
+    Note that stderr is opened unbuffered, so if it shares a file with
+    stdout then interleaved output may not appear in the order that you
+    expect."""
+    # Do first fork.
+    try:
+        pid = os.fork()
+        if pid > 0:
+            # Exit first parent.
+            sys.exit(0)
+    except OSError as err:
+        print("Fork #1 failed: (%d) %s" % (err.errno, err.strerror),
+              file=sys.stderr)
+        sys.exit(1)
+
+    # Decouple from parent environment.
+    os.chdir("/")
+    os.umask(0)
+    os.setsid()
+
+    # Do second fork.
+    try:
+        pid = os.fork()
+        if pid > 0:
+            # Exit second parent.
+            sys.exit(0)
+    except OSError as err:
+        print("Fork #2 failed: (%d) %s" % (err.errno, err.strerror),
+              file=sys.stderr)
+        sys.exit(1)
+
+    # Open file descriptors and print start message.
+    if not stderr:
+        stderr = stdout
+    stdi = open(stdin, "r")
+    stdo = open(stdout, "a+")
+    stde = open(stderr, "ab+", 0)
+    pid = str(os.getpid())
+    if pidfile:
+        with open(pidfile, "w+") as pidf:
+            pidf.write("%s\n" % pid)
+
+    # Redirect standard file descriptors.
+    os.dup2(stdi.fileno(), sys.stdin.fileno())
+    os.dup2(stdo.fileno(), sys.stdout.fileno())
+    os.dup2(stde.fileno(), sys.stderr.fileno())
 
 
 def main():
@@ -51,6 +108,8 @@ def main():
     args = parser.parse_args()
     logger = pad.config.setup_logging("pad-logger", debug=args.debug,
                                       filepath=args.log_file)
+    if args.daemonize:
+        detach(pidfile=args.pidfile)
     address = (args.listen, args.port)
     if args.prefork is not None:
         server = pad.server.PreForkServer(address, args.sitepath,
@@ -59,7 +118,13 @@ def main():
     else:
         server = pad.server.Server(address, args.sitepath, args.configpath,
                                    args.paranoid)
-    server.serve_forever()
+    try:
+        server.serve_forever()
+    finally:
+        try:
+            os.remove(args.pidfile)
+        except OSError:
+            pass
 
 
 if __name__ == "__main__":
