ddbabb423d201814a03f754addc503e9308367a9
Marco Antonio Islas Cruz <markuz@islascruz.org>
2016-01-18 08:34:27 -0600

    Refs #5 - Use Python 3 ipaddress or Python2 py2-ipaddress to check if the ipaddress is private

--- pad/plugins/relay_country.py
+++ pad/plugins/relay_country.py
@@ -3,31 +3,27 @@
 from __future__ import absolute_import
 
 import re
-import socket
-import struct
 
 import pad.errors
+
 try:
     import geoip2.database
 except ImportError:
     raise pad.errors.PluginLoadError(
-            "RelayCountryPlugin not loaded, You must install geoip2 to use this plugin")
+        "RelayCountryPlugin not loaded, You must install geoip2 to use "
+        "this plugin")
+try:
+    import ipaddress
+except ImportError:
+    raise pad.errors.PluginLoadError(
+        "RelayCountryPlugin not loaded, You must install py2-ipaddress to "
+        "use this plugin")
+
 
 import pad.plugins.base
 
 IPFRE = re.compile(r"[\[\(\s\/]((?:[0-9]{1,3}\.){3}[0-9]{1,3})[\s\]\)\/]")
-LOWER_172_IP = 2886729728
-UPPER_172_IP = 2887778303
 
-def ip2long(ip):
-    """
-    Convert an IP string to long
-    """
-    try:
-        packedIP = socket.inet_aton(ip)
-        return struct.unpack("!L", packedIP)[0]
-    except (socket.error, struct.error):
-        pass
 
 class RelayCountryPlugin(pad.plugins.base.BasePlugin):
     """This plugin exposes the countries that a mail was relayed from.
@@ -53,23 +49,17 @@ class RelayCountryPlugin(pad.plugins.base.BasePlugin):
         except IOError as exc:
             self.ctxt.log.warning("Unable to open geo database file: %r", exc)
 
-    def get_country(self, ipaddress):
+    def get_country(self, ipaddr):
         """Return the country corresponding to an IP based on the
         network range database.
         """
-        if (ipaddress.startswith("10") or
-                ipaddress.startswith("192.168.") or
-                ipaddress.startswith("127.0.0.1")):
+        ipadobj = ipaddress.ip_address(ipaddr)
+        if ipadobj.is_private:
             return "**"
-        if ipaddress.startswith("172"):
-            #The 20-bit block goes from 172.16.0.0 to 172.32.255.255
-            longip = ip2long(ipaddress)
-            if longip >= LOWER_172_IP and longip <= UPPER_172_IP:
-                return "**"
         try:
-            response = self.reader.country(ipaddress)
+            response = self.reader.country(ipaddr)
         except geoip2.errors.AddressNotFoundError:
-            self.ctxt.log.info("Can't locate IP '%s' in database", ipaddress)
+            self.ctxt.log.info("Can't locate IP '%s' in database", ipaddr)
             #Cant locate the IP in database.
             return "XX"
         return response.country.iso_code # pylint: disable=maybe-no-member
@@ -91,8 +81,8 @@ class RelayCountryPlugin(pad.plugins.base.BasePlugin):
         ips = IPFRE.findall(all_received)
         result = []
         self.ctxt.log.debug("IPS found: %r", ips)
-        for ipaddress in ips:
-            country = self.get_country(ipaddress)
+        for ipaddr in ips:
+            country = self.get_country(ipaddr)
             result.append(str(country))
         if result:
             msg.headers["X-Relay-Country"].append(" ".join(result))
