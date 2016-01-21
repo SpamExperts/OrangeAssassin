038accfe1bd88787ab50de80f8b78ab6504e7ae2
Alexandru Chirila <alex@alexkiro.com>
2016-01-21 10:24:39 +0200

    Style fixes.

--- tests/util/sample_plugin.py
+++ tests/util/sample_plugin.py
@@ -1,24 +1,21 @@
-"""Similar to the DumpText demo SA plugin."""
+"""Sample plugin that can be used for testing."""
 
 from __future__ import print_function, absolute_import
-
-import sys
 
 import pad.plugins.base
 
 
 class TestPluginReportRevoke(pad.plugins.base.BasePlugin):
-    """Similar to the SA DumpText demo plugin, useful for debugging rulesets.
-    """
+    """Sample plugin that can be used for testing."""
     options = {}
     eval_rules = ()
 
     def plugin_report(self, msg):
-        """Report the digest to pyzor as spam."""
+        """Report the message as spam."""
         super(TestPluginReportRevoke, self).plugin_report(msg)
         print("Reporting message.")
 
     def plugin_revoke(self, msg):
-        """Report the digest to pyzor as ham."""
+        """Report the message as ham."""
         super(TestPluginReportRevoke, self).plugin_revoke(msg)
         print("Revoking message.")
