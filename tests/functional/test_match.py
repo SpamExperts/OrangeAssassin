fe5e8e8ac5d010ef08694f9b067451c7559fda67
Alexey Anisimov <aragorna@gmail.com>
2016-01-21 00:20:05 -0600

    Refs. issue #2. Add more functional tests for match.py script

--- tests/functional/test_match.py
+++ tests/functional/test_match.py
@@ -6,6 +6,34 @@ import tests.util
 
 GTUBE = "XJS*C4JDBQADN1.NSBN3*2IDNEN*GTUBE-STANDARD-ANTI-UBE-TEST-EMAIL*C.34X"
 
+MULTIPART_MSG = r"""From: Marco Antonio Islas Cruz <marco@seinternal.com>
+Content-Type: multipart/alternative;
+    boundary="Apple-Mail=_9311E301-2E56-423D-B730-30A522F3844C"
+X-Smtp-Server: smtp.gmail.com:marco@seinternal.com
+Subject: Non text email
+X-Universally-Unique-Identifier: 6c318f30-bec6-49cf-a37c-e651b9ce970e
+Message-Id: <FC768970-9D08-4702-B0BF-9ED7A21F9D97@islascruz.org>
+To: Marco antonio Islas Cruz <marco@seinternal.com>
+Mime-Version: 1.0 (Apple Message framework v1257)
+
+
+--Apple-Mail=_9311E301-2E56-423D-B730-30A522F3844C
+Content-Type: multipart/related;
+    type="text/html";
+    boundary="Apple-Mail=_7F2342CA-8904-478A-B198-D63EE91D8288"
+
+
+--Apple-Mail=_7F2342CA-8904-478A-B198-D63EE91D8288
+Content-Transfer-Encoding: 7bit
+Content-Type: text/html;
+    charset=us-ascii
+
+<html><head></head><body style="word-wrap: break-word; -webkit-nbsp-mode: space; -webkit-line-break: after-white-space; "><div>This is a test?</div><div><br></div><img id="7c666adc-282a-46d4-9f3c-adce8a02b0be" height="339" width="530" apple-width="yes" apple-height="yes" src="cid:40808429-84C6-4DB6-982E-451F05730FE0@ubuntu"><br><br>
+Testing rule one-two-three
+<br></body></html>
+--Apple-Mail=_9311E301-2E56-423D-B730-30A522F3844C--
+"""
+
 
 class TestBodyRules(tests.util.TestBase):
 
@@ -22,16 +50,88 @@ class TestBodyRules(tests.util.TestBase):
         self.assertEqual(result, "")
 
     def test_match(self):
-        """Rule should be matched and reported"""
-        self.setup_conf(config="body TEST_RULE /abcd/", pre_config="report _SCORE_")
+        """Rule should be matched and score reported"""
+        self.setup_conf(config="body TEST_RULE /abcd/",
+                        pre_config="report _SCORE_")
         result = self.check_pad("Subject: test\n\nTest abcd test.")
         self.assertEqual(result, "1.0")
 
+    def test_no_match(self):
+        """Rule shouldn't be matched but score reported"""
+        self.setup_conf(config="body TEST_RULE /abddd/",
+                        pre_config="report _SCORE_")
+        result = self.check_pad("Subject: test\n\nTest abcd test.")
+        self.assertEqual(result, "0.0")
+
     def test_gtube(self):
-        """Gtube should be matched and reported"""
+        """Gtube should be matched"""
         self.setup_conf(pre_config="report _SCORE_")
         result = self.check_pad("Subject: test\n\n" + GTUBE)
         self.assertEqual(result, "1000.0")
 
+    def test_match_revoke(self):
+        """Rule should be matched and revoked"""
+        self.setup_conf(config="body TEST_RULE /abcd/",
+                        pre_config="report _SCORE_")
+        result = self.check_pad("Subject: test\n\nTest abcd test.",
+                                report_only=False, extra_args=["--revoke", ])
+        self.assertEqual(result, "1 message(s) examined")
 
+    def test_match_report(self):
+        """Rule should be matched and reported"""
+        self.setup_conf(config="body TEST_RULE /abcd/",
+                        pre_config="report _SCORE_")
+        result = self.check_pad("Subject: test\n\nTest abcd test.",
+                                report_only=False, extra_args=["--report", ])
+        self.assertEqual(result, "1 message(s) examined")
 
+    def test_match_show_unknown(self):
+        """Rule should be matched with show-unknown option"""
+        self.setup_conf(config="body TEST_RULE /abcd/",
+                        pre_config="report _SCORE_")
+        result = self.check_pad("Subject: test\n\nTest abcd test.",
+                                report_only=True, extra_args=["--show-unknown", ])
+        self.assertEqual(result, "1.0")
+
+    def test_match_show_paranoid(self):
+        """Rule should be matched with paranoid option"""
+        self.setup_conf(config="body TEST_RULE /abcd/",
+                        pre_config="report _SCORE_")
+        result = self.check_pad("Subject: test\n\nTest abcd test.",
+                                report_only=True, extra_args=["--paranoid", ])
+        self.assertEqual(result, "1.0")
+
+    def test_match_multipart(self):
+        """Rule should be matched with multipart message"""
+        self.setup_conf(config="body TEST_RULE /one-two-three/",
+                        pre_config="report _SCORE_")
+        result = self.check_pad(MULTIPART_MSG)
+        self.assertEqual(result, "1.0")
+
+    def test_no_match_multipart(self):
+        """Rule shouldn't be matched with multipart message"""
+        self.setup_conf(config="body TEST_RULE /one-two-four/",
+                        pre_config="report _SCORE_")
+        result = self.check_pad(MULTIPART_MSG)
+        self.assertEqual(result, "0.0")
+
+    def test_match_several_rules(self):
+        """Rule should be matched and score reported"""
+        self.setup_conf(
+            config="body TEST_RULE /abcd/\n"
+                   "body GTUBE /XJS\*C4JDBQADN1\.NSBN3\*2IDN"
+                   "EN\*GTUBE-STANDARD-ANTI-UBE-TEST-EMAIL\*C\.34X/",
+            pre_config="report _SCORE_")
+        result = self.check_pad(
+            "Subject: test\n\nTest abcd test. {0}".format(GTUBE))
+        self.assertEqual(result, "2.0")
+
+    def test_no_match_several_rules(self):
+        """Rule shouldn't be matched but score reported"""
+        self.setup_conf(
+            config="body TEST_RULE /abcd/\n"
+                   "body GTUBE /XJS\*C4JDBQADN1\.NSBN3\*2IDN"
+                   "EN\*GTUBE-STANDARD-ANTI-UBE-TEST-EMAIL\*C\.34X/",
+            pre_config="report _SCORE_")
+        result = self.check_pad(MULTIPART_MSG)
+        self.assertEqual(result, "0.0")
