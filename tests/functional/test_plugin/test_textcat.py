01c0643670ec0d31f694436cbca4f8561e3e0250
Psy <psycho@localhost.localdomain>
2016-01-26 14:34:04 +0200

    Refs #7 - Added a test scenario to check the textcat plugin

---
+++ tests/functional/test_plugin/test_textcat.py
@@ -0,0 +1,34 @@
+"""Tests the TextCat Plugin"""
+
+from __future__ import absolute_import
+import unittest
+import tests.util
+
+LANG_CONFIG = """
+body UNWANTED_LANGUAGE_BODY	eval:check_language()
+describe UNWANTED_LANGUAGE_BODY	Message written in an undesired language
+score UNWANTED_LANGUAGE_BODY 2.800
+"""
+PRE_CONFIG = """
+loadplugin     Mail::SpamAssassin::Plugin::TextCat
+ok_languages   %s
+"""
+
+
+class TestFunctionalTextCat(tests.util.TestBase):
+
+    def test_ok_languages(self):
+        self.setup_conf(config=LANG_CONFIG,
+                        pre_config=PRE_CONFIG % "ro")
+        result = self.check_pad("Subject: test\n\nTest abcd test.")
+        self.check_report(result, 2.8, ["UNWANTED_LANGUAGE_BODY"])
+
+def suite():
+    """Gather all the tests from this package in a test suite."""
+    test_suite = unittest.TestSuite()
+    test_suite.addTest(unittest.makeSuite(TestFunctionalTextCat, "test"))
+    return test_suite
+
+
+if __name__ == '__main__':
+    unittest.main(defaultTest='suite')
