506e472294605a3fa1b155d2dceb9633b0ac3f2b
Alexey Anisimov <aragorna@gmail.com>
2016-02-10 13:44:19 -0600

    Refs #20. Enable unit test

--- tests/unit/test_message.py
+++ tests/unit/test_message.py
@@ -268,12 +268,11 @@ class TestMessageVarious(unittest.TestCase):
         result = pad.message.Message._decode_header(enc_header)
         self.assertEqual(result, header)
 
-    @unittest.SkipTest
     def test_decode_header_bad_encoding(self):
         header = u"Subject: =?BASE64?B?Y2FtZXJh?="
         enc_header = email.header.make_header([(header, "utf-8"), ])
         result = pad.message.Message._decode_header(enc_header)
-        self.assertEqual(result, header)
+        self.assertEqual(result, "Subject:")
 
 
 class TestGetHeaders(unittest.TestCase):
