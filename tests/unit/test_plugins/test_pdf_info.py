8f6b632bcaf580c5883ff81f6399cf764caeaef3
Marco Antonio Islas Cruz <markuz@islascruz.org>
2016-02-12 03:42:11 -0600

    Added first set of unittests

---
+++ tests/unit/test_plugins/test_pdf_info.py
@@ -0,0 +1,247 @@
+"""Tests for pad.plugins.pdf_info."""
+import unittest
+
+from hashlib import md5
+from tests.util.pdf_utils import new_email, new_pdf, PDFWithAttachments
+from tests.util.image_utils import new_image_string
+
+try:
+    from unittests.mock import patch, Mock, MagicMock, call
+except ImportError:
+    from mock import patch, Mock, MagicMock, call
+
+import pad.plugins
+
+class PDFInfoBase(unittest.TestCase):
+    """Test for the PDFInfo plugin"""
+    def setUp(self):
+        unittest.TestCase.setUp(self)
+        self.options = {}
+        self.global_data = {}
+        self.msg_data = {}
+        patch("pad.plugins.pdf_info.PDFInfoPlugin.options",
+              self.options).start()
+        self.mock_ctxt = MagicMock(**{
+            "get_plugin_data.side_effect": lambda p, k: self.msg_data[k],
+            "set_plugin_data."
+            "side_effect": lambda p, k, v: self.msg_data.setdefault(k, v),
+            })
+        self.mock_msg = MagicMock(**{
+            "get_plugin_data.side_effect": lambda p, k: self.msg_data[k],
+            "set_plugin_data."
+            "side_effect": lambda p, k, v: self.msg_data.update({k:v}),
+        })
+        self.mock_msg.msg = None
+        self.plugin = pad.plugins.pdf_info.PDFInfoPlugin(self.mock_ctxt)
+
+    def tearDown(self):
+        unittest.TestCase.tearDown(self)
+        patch.stopall()
+
+
+class TestPDFInfo(PDFInfoBase):
+    def test_extract_metadata(self):
+        patch("pad.plugins.pdf_info.PDFInfoPlugin._add_name").start()
+        patch("pad.plugins.pdf_info.PDFInfoPlugin._update_counts").start()
+        patch("pad.plugins.pdf_info.PDFInfoPlugin._save_stats").start()
+
+        add_name_calls = []
+        update_counts_calls = []
+        save_stats_calls = []
+        update_details_calls= []
+        update_image_count_calls = []
+        update_pixel_coverage_calls = []
+        pdfs = {}
+        allpdfinfo = (
+                {"details": {"/Author":"Author1", "/Creator":"unittest","/Created":"2016-02-11",
+                    "/Title": "pdftest"}},
+                {"details": {"/Author":"Author2", "/Creator":"unittest","/Created":"2016-02-11",
+                    "/Title": "pdftest2"}},
+                {"details": {"/Author":"Author3", "/Creator":"unittest","/Created":"2016-02-11",
+                    "/Title": "pdftest3"}},
+                {"details": {"/Author":"Author4", "/Creator":"unittest","/Created":"2016-02-11",
+                    "/Title": "pdftest4"}, "images": ("image1", (100,100))},
+                )
+        for x in xrange(len(allpdfinfo)):
+            pdfinfo = allpdfinfo[x]
+            name = "%d.pdf" % x
+            if "images" not in pdfinfo:
+                pdfobj = new_pdf(details = pdfinfo["details"], name=name)
+            else:
+                pdfc = PDFWithAttachments(details = pdfinfo["details"], name=name)
+                image = new_image_string(pdfinfo["images"][1])
+                pdfc.addAttachment(pdfinfo["images"][0], image)
+                pdfobj = {"data": pdfc.as_file(), "name": name}
+            pdfs.update({x: pdfobj})
+            add_name_calls.append(call(self.mock_msg, name))
+            update_counts_calls.append(call(self.mock_msg, incr=1))
+            save_stats_calls.append(call(self.mock_msg, pdfobj["data"].read()))
+
+        self.mock_msg.msg = new_email(pdfs)
+
+        for part in self.mock_msg.msg.walk():
+            payload = part.get_payload(decode=True)
+            self.plugin.extract_metadata(self.mock_msg, payload, part)
+
+        self.plugin._add_name.assert_has_calls(add_name_calls)
+        self.plugin._update_counts.assert_has_calls(update_counts_calls)
+        self.plugin._save_stats.assert_has_calls(save_stats_calls)
+
+    @unittest.skip("Temporary disabled, The expected is not matching the result")
+    def test_update_stats(self):
+        patch("pad.plugins.pdf_info.PDFInfoPlugin._update_details").start()
+        patch("pad.plugins.pdf_info.PDFInfoPlugin._update_image_counts").start()
+        patch("pad.plugins.pdf_info.PDFInfoPlugin._update_pixel_coverage").start()
+
+        update_details_calls= []
+        update_image_count_calls = []
+        update_pixel_coverage_calls = []
+        pdfs = {}
+        allpdfinfo = (
+                {"details": {"/Author":"Author1", "/Creator":"unittest","/Producer":"2016-02-11",
+                    "/Title": "pdftest"}},
+                {"details": {"/Author":"Author2", "/Creator":"unittest","/Producer":"2016-02-11",
+                    "/Title": "pdftest2"}},
+                {"details": {"/Author":"Author3", "/Creator":"unittest","/Producer":"2016-02-11",
+                    "/Title": "pdftest3"}},
+                {"details": {"/Author":"Author4", "/Creator":"unittest","/Producer":"2016-02-11",
+                    "/Title": "pdftest4"}, "images": ("image1", (100,100))},
+                )
+        for x in xrange(len(allpdfinfo)):
+            pdfinfo = allpdfinfo[x]
+            name = "%d.pdf" % x
+            if "images" not in pdfinfo:
+                pdfobj = new_pdf(details = pdfinfo["details"], name=name)
+            else:
+                pdfc = PDFWithAttachments(details = pdfinfo["details"], name=name)
+                image = new_image_string(pdfinfo["images"][1])
+                pdfc.addAttachment(pdfinfo["images"][0], image)
+                pdfobj = {"data": pdfc.as_file(), "name": name}
+            pdfs.update({x: pdfobj})
+            pdf_id = md5(pdfobj["data"].getvalue()).hexdigest()
+            for det in pdfinfo["details"]:
+                value  = unicode(pdfinfo["details"][det])
+                update_details_calls.append(call(self.mock_msg, pdf_id, det.lower()[1:],
+                    value))
+            if "images" in pdfinfo:
+                update_image_count_calls.append(call(self.mock_msg, incr = 1))
+                width, height = pdfinfo["images"][1]
+                update_pixel_coverage_calls.append(call(self.mock_msg, width * height))
+
+        self.mock_msg.msg = new_email(pdfs)
+
+        for part in self.mock_msg.msg.walk():
+            payload = part.get_payload(decode=True)
+            if payload is None:
+                continue
+            self.plugin._save_stats(self.mock_msg, payload)
+
+        self.plugin._update_details.assert_has_calls(update_details_calls)
+        self.plugin._update_image_counts.assert_has_calls(update_image_count_calls)
+        self.plugin._update_pixel_coverage.assert_has_calls(update_pixel_coverage_calls)
+
+    def test_pdf_count(self):
+        """Test the pdf_count"""
+        self.plugin.set_local(self.mock_msg, "counts", 1)
+        self.assertEqual(self.plugin._get_count(self.mock_msg), 1)
+
+    def test_pdf_count_zero(self):
+        """Test the pdf_count when there are no images"""
+        self.assertEqual(self.plugin._get_count(self.mock_msg), 0)
+
+    def test_update_pdf_count(self):
+        """Test updating the image count"""
+        expected = 10
+        self.plugin._update_counts(self.mock_msg, incr = 1)
+        self.plugin._update_counts(self.mock_msg, incr = 2)
+        self.plugin._update_counts(self.mock_msg, incr = 7)
+        self.assertEqual(self.plugin.get_local(self.mock_msg, 
+            "counts"), expected)
+
+    def test_pdf_count_match(self):
+        """Test pdf_image_count (extract the images from the PDF and count them)
+        """
+        self.plugin.set_local(self.mock_msg, "counts", 3)       
+        self.assertTrue(self.plugin.pdf_count(self.mock_msg,3, 5))
+    
+    def test_image_count(self):
+        """Test the pdf_image_count"""
+        self.plugin.set_local(self.mock_msg, "image_counts", 1)
+        self.assertEqual(self.plugin._get_image_count(self.mock_msg), 1)
+
+    def test_image_count_zero_images(self):
+        """Test the pdf_image_count when there are no images"""
+        self.assertEqual(self.plugin._get_image_count(self.mock_msg), 0)
+
+    def test_update_image_count(self):
+        """Test updating the image count"""
+        expected = 10
+        self.plugin._update_image_counts(self.mock_msg, incr = 1)
+        self.plugin._update_image_counts(self.mock_msg, incr = 2)
+        self.plugin._update_image_counts(self.mock_msg, incr = 7)
+        self.assertEqual(self.plugin.get_local(self.mock_msg, 
+            "image_counts"), expected)
+
+    def test_pdf_image_count(self):
+        """Test pdf_image_count (extract the images from the PDF and count them)
+        """
+        self.plugin.set_local(self.mock_msg, "image_counts", 3)       
+        self.assertTrue(self.plugin.pdf_image_count(self.mock_msg,3, 5))
+
+    def test_pixel_coverage(self):
+        """Test the pdf_pixel_coverage"""
+        self.plugin.set_local(self.mock_msg, "pixel_coverage", 100)
+        self.assertEqual(self.plugin._get_pixel_coverage(self.mock_msg), 100)
+
+    def test_pixel_coverage(self):
+        """Test the pdf_pixel_coverage when there are no images"""
+        self.assertEqual(self.plugin._get_pixel_coverage(self.mock_msg), 0)
+
+    def test_update_pixel_coverage(self):
+        """Test updating the image count"""
+        expected = 1000
+        self.plugin._update_pixel_coverage(self.mock_msg, incr = 100)
+        self.plugin._update_pixel_coverage(self.mock_msg, incr = 200)
+        self.plugin._update_pixel_coverage(self.mock_msg, incr = 700)
+        self.assertEqual(self.plugin.get_local(self.mock_msg, 
+            "pixel_coverage"), expected)
+
+    def test_pdf_pixel_coverage(self):
+        """Test pdf_image_count (extract the images from the PDF and count them)
+        """
+        self.plugin.set_local(self.mock_msg, "pixel_coverage", 3)       
+        self.assertTrue(self.plugin.pdf_pixel_coverage(self.mock_msg,3, 5))
+
+    def test_add_name(self):
+        """Test the pdf_add_name"""
+        self.plugin.set_local(self.mock_msg, "names", "pdf.pdf")
+        self.assertEqual(self.plugin._get_pdf_names(self.mock_msg), "pdf.pdf")
+
+    def test_pdf_named_no_name(self):
+        """Test the pdf_pixel_coverage when there are no images"""
+        self.assertEqual(self.plugin._get_pdf_names(self.mock_msg), [])
+
+    def test_pdf_add_name(self):
+        """Test updating the image count"""
+        expected = set(["first", "second", "third"])
+        self.plugin._add_name(self.mock_msg, name = "first")
+        self.plugin._add_name(self.mock_msg, name = "second")
+        self.plugin._add_name(self.mock_msg, name = "third")
+        self.assertEqual(self.plugin.get_local(self.mock_msg, 
+            "names"), expected)
+
+    def test_pdf_named(self):
+        """Test pdf_image_count (extract the images from the PDF and count them)
+        """
+        self.plugin.set_local(self.mock_msg, "names", set(["first"]))       
+        self.assertTrue(self.plugin.pdf_pixel_coverage(self.mock_msg, "first"))
+
+def suite():
+    """Gather all the tests from this module in a test suite."""
+    test_suite = unittest.TestSuite()
+    test_suite.addTest(unittest.makeSuite(TestPDFInfo, "test"))
+    return test_suite
+
+if __name__ == "__main__":
+    unittest.main(defaultTest="suite")
+
