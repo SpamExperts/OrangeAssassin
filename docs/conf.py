fe32943638a71d30f646a772f87294c6a05ec273
Alexandru Chirila <alex@alexkiro.com>
2016-02-17 12:17:44 +0200

    Add requirements for documentation.
    Also add a small script that generates a documentation
    base for any plugin based on the template.

--- docs/conf.py
+++ docs/conf.py
@@ -32,8 +32,8 @@ extensions = [
     'sphinx.ext.autodoc',
     'sphinx.ext.todo',
     'sphinx.ext.coverage',
-    'sphinx.ext.viewcode',
-    'sphinx.ext.linkcode',
+    # 'sphinx.ext.viewcode',
+    # 'sphinx.ext.linkcode',
 ]
 
 # Add any paths that contain templates here, relative to this directory.
