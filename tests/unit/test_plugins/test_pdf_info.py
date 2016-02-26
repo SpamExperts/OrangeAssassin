b06f12344c2f7ac63928c26d2310695351aa33a8
Alexey Anisimov <aragorna@gmail.com>
2016-02-26 20:59:01 +0100

    Refs. #9. Add more unit tests

--- tests/unit/test_plugins/test_pdf_info.py
+++ tests/unit/test_plugins/test_pdf_info.py
@@ -407,6 +407,21 @@ class TestPDFDetails(PDFInfoBase):
         self.assertFalse(self.plugin.pdf_match_details(self.mock_msg, "author",
                                                        r"/^xxx\w{1,9}$/i"))
 
+    @unittest.SkipTest
+    def test_pdf_match_details_none(self):
+        """Test the match_details method when value = None"""
+        pdf_id = "1234567890"
+        self.plugin._update_details(self.mock_msg, pdf_id, "author", None)
+        self.assertTrue(self.plugin.pdf_match_details(self.mock_msg, "author",
+                                                      r"/^tes\w{1,9}$/i"))
+
+    def test_pdf_match_details_bad_regex(self):
+        """Test the match_details method when regex is wrong"""
+        pdf_id = "1234567890"
+        self.plugin._update_details(self.mock_msg, pdf_id, "author", "test")
+        with self.assertRaises(pad.errors.InvalidRegex):
+            self.plugin.pdf_match_details(self.mock_msg, "author", r".*")
+
 
 class TestPDFEncrypted(PDFInfoBase):
     """Tests for encrypted PDFs"""
