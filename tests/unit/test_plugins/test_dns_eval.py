df4b9ec338da58c197f1559d14264fd942886e3b
Alexandru Chirila <alex@alexkiro.com>
2016-03-02 14:37:34 +0200

    Refs. #26. Add some unit-tests.

--- tests/unit/test_plugins/test_dns_eval.py
+++ tests/unit/test_plugins/test_dns_eval.py
@@ -7,6 +7,8 @@ try:
 except ImportError:
     from mock import patch, Mock, MagicMock, call
 
+import ipaddress
+
 import pad.context
 import pad.plugins.dns_eval
 
@@ -15,25 +17,48 @@ class TestDNSEval(unittest.TestCase):
 
     def setUp(self):
         unittest.TestCase.setUp(self)
-        self.ips = ["127.0.0.1"]
+        self.ips = [ipaddress.ip_address(u"127.0.0.1")]
         self.local_data = {}
         self.global_data = {}
         self.mock_ctxt = MagicMock()
-        self.mock_ctxt.reverse_ip = pad.context.GlobalContext.reverse_ip
+        self.mock_ctxt.reverse_ip = pad.context.GlobalContext().reverse_ip
         self.mock_msg = MagicMock()
+        self.mock_msg.sender_address = "sender@example.com"
         self.mock_msg.get_untrusted_ips.return_value = self.ips
         self.plugin = pad.plugins.dns_eval.DNSEval(self.mock_ctxt)
         self.plugin.set_local = lambda m, k, v: self.local_data.__setitem__(k, v)
         self.plugin.get_local = lambda m, k: self.local_data.__getitem__(k)
         self.plugin.set_global = self.global_data.__setitem__
         self.plugin.get_global = self.global_data.__getitem__
-        self.mock_ruleset = MagicMock()
+        self.mock_ruleset = MagicMock(checked={}, not_checked={})
 
     def test_finish_parsing_end(self):
-        pass
+        eval_rule = MagicMock()
+        eval_rule.eval_rule_name = "check_rbl"
+        eval_rule.eval_args = ("set id", "rbl.example.com.")
+        self.mock_ruleset.checked["MY_RULE"] = eval_rule
+        patch("pad.plugins.dns_eval.isinstance", return_value=True,
+              create=True).start()
 
-    @unittest.skip("Not ready.")
+        self.plugin.finish_parsing_end(self.mock_ruleset)
+        self.assertEqual(
+            self.plugin["zones"], {"set id": "rbl.example.com."}
+        )
+
     def test_check_rbl(self):
-        self.plugin._check_rbl(self.mock_msg, "example.com")
+        """Test the check_rbl method."""
+        self.plugin.check_rbl(
+            self.mock_msg, "example_ser", "example.com"
+        )
         self.mock_ctxt.query_dns.assert_called_with(
-            "1.0.0.127.example.com", )
+            "1.0.0.127.example.com", 'A')
+
+    def test_check_rbl_from_domain(self):
+        """Test the check_rbl_from_domain eval rule"""
+        from_headers = ["test@example.org"]
+        self.mock_msg.get_addr_header.return_value = from_headers
+        self.plugin.check_rbl_from_domain(
+            self.mock_msg, "example_set", "example.com"
+        )
+        self.mock_ctxt.query_dns.assert_called_with(
+            "example.org.example.com", 'A')
