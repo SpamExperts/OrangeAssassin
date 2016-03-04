e1ba6db3f2efba4401eccfe4bf09560fea5026dc
kubbity <kubbity@gmail.com>
2016-03-04 05:21:36 -1000

    Refs. #26 - Add more unit-tests for DNSEval plugin.

--- tests/unit/test_plugins/test_dns_eval.py
+++ tests/unit/test_plugins/test_dns_eval.py
@@ -53,6 +53,90 @@ class TestDNSEval(unittest.TestCase):
         self.mock_ctxt.query_dns.assert_called_with(
             "1.0.0.127.example.com", 'A')
 
+    def test_check_rbl_subnet(self):
+        """Test the check_rbl method."""
+        self.plugin.check_rbl(
+            self.mock_msg, "example_ser", "example.com", "127.0.0.1"
+        )
+        self.mock_ctxt.query_dns.assert_called_with(
+            "1.0.0.127.example.com", 'A')
+
+    def test_check_rbl_txt(self):
+        """Test the check_rbl_txt method."""
+        self.plugin.check_rbl_txt(
+            self.mock_msg, "example_ser", "example.com", "127.0.0.2"
+        )
+        self.mock_ctxt.query_dns.assert_called_with(
+            "1.0.0.127.example.com", 'TXT')
+
+    def test_check_rbl_sub(self):
+        """Test the check_rbl_sub method."""
+        self.plugin.check_rbl(
+            self.mock_msg, "example_ser", "example.com",
+        )
+        self.plugin.check_rbl_sub(
+            self.mock_msg, "example_ser", "127.0.0.1",
+        )
+        self.mock_ctxt.query_dns.assert_called_with(
+            "1.0.0.127.example.com", 'A')
+
+    def test_check_dns_sender_with_a_records(self):
+        """Test the check_dns_sender rule"""
+
+        def mock_query(domain, rtype="A"):
+            return ["127.0.0.1"]
+
+        self.mock_ctxt.query_dns.side_effect = mock_query
+        restult = self.plugin.check_dns_sender(self.mock_msg)
+        self.assertFalse(restult)
+
+    def test_check_dns_sender_no_mx(self):
+        """Test the check_dns_sender rule"""
+
+        def mock_query_a(domain, rtype="A"):
+            return []
+
+        def mock_query_mx(domain, rtype="MX"):
+            return ["127.0.0.1"]
+
+        self.mock_ctxt.query_dns.side_effect = mock_query_a
+        self.mock_ctxt.query_dns.side_effect = mock_query_mx
+        restult = self.plugin.check_dns_sender(self.mock_msg)
+        self.assertFalse(restult)
+
+    def test_check_dns_sender_invalid(self):
+        """Test the check_dns_sender rule"""
+
+        def mock_query_a(domain, rtype="A"):
+            return ["127.0.0.1"]
+
+        def mock_query_mx(domain, rtype="MX"):
+            return []
+
+        self.mock_ctxt.query_dns.side_effect = mock_query_a
+        self.mock_ctxt.query_dns.side_effect = mock_query_mx
+
+        result = self.plugin.check_dns_sender(self.mock_msg)
+        self.assertTrue(result)
+
+    def test_check_rbl_envfrom(self):
+        """Test the check_rbl_from_envfrom eval rule"""
+        self.plugin.check_rbl_envfrom(
+            self.mock_msg, "example_set", "example.org"
+        )
+        self.mock_ctxt.query_dns.assert_called_with(
+            "example.com.example.org", 'A')
+
+    def test_check_rbl_from_host(self):
+        """Test the check_rbl_from_domain eval rule"""
+        from_host = ["test@example.net"]
+        self.mock_msg.get_addr_header.return_value = from_host
+        self.plugin.check_rbl_from_host(
+            self.mock_msg, "example_set", "example.com"
+        )
+        self.mock_ctxt.query_dns.assert_called_with(
+            "example.net.example.com", 'A')
+
     def test_check_rbl_from_domain(self):
         """Test the check_rbl_from_domain eval rule"""
         from_headers = ["test@example.org"]
@@ -62,3 +146,12 @@ class TestDNSEval(unittest.TestCase):
         )
         self.mock_ctxt.query_dns.assert_called_with(
             "example.org.example.com", 'A')
+
+    def test_check_rbl_accreditor(self):
+        """Test the check_rbl_from_domain eval rule"""
+        self.mock_msg.sender_address = "sender@a--accreditor.mail.example.com"
+        self.plugin.check_rbl_accreditor(
+            self.mock_msg, "accredit", "example.com", "127.0.0.1", "accreditor"
+        )
+        self.mock_ctxt.query_dns.assert_called_with(
+            "1.0.0.127.example.com", 'A')
