71bb31d1c1fab4fe01b79ee32ac1bb087eac0690
ccatalinstoian <stoian@gapps.spamexperts.com>
2016-03-08 16:33:43 +0300

    Refs #42. Added functional test for priority option

---
+++ tests/functional/test_rules/test_priority.py
@@ -0,0 +1,55 @@
+"""Tests priority rules."""
+from __future__ import absolute_import
+
+import tests.util
+
+PRE_CONFIG = """
+report _SCORE_
+report _TESTS_
+"""
+
+CONFIG = r"""
+# Rule definitions here
+
+body UNLIMITED /unlimited/i
+score UNLIMITED 10.45
+
+body EMAIL /email/
+score EMAIL -12.15
+priority EMAIL 5
+
+body SPAM_TEST /spam_test/i
+score SPAM_TEST 55.12
+
+body SPAM_ON /spam_on/i
+score SPAM_ON 25.73
+
+body HAM_TEST /ham_test/
+score HAM_TEST -75.3
+
+body HAM_ON /ham_on/i
+score HAM_ON -35.01
+
+body TEST_RULE /abcd/
+score TEST_RULE 2.5
+priority TEST_RULE 2
+"""
+
+MSG = """Received: from 127.0.0.1  (EHLO m1.util24.eu) [213.133.99.176]
+  by mta1329.mail.bf1.yahoo.com with SMTPS; Fri, 29 Jan 2016 05:57:28 +0000
+Subject: Test Message.
+X-Header: match_header
+
+This is a test message for the Short Circuit Plugin.
+Adjust the body for your case: %s
+"""
+
+
+class TestPriorityRules(tests.util.TestBase):
+
+    def test_priority_rule_match(self):
+        """Test the priority for rules"""
+        self.setup_conf(config=CONFIG, pre_config=PRE_CONFIG)
+        result = self.check_pad(MSG % "abcd email", debug=True)
+        print(result)
+        self.check_report(result, -8.7, ["EMAIL", "TEST_RULE"])
