a3d5849579c6d3fdfb5866a63acabb52cd93d063
Cosmin Badea <cosminBadea@localhost.localdomain>
2016-09-08 15:43:46 +0300

    Ref #39 Add more functional tests

--- tests/functional/test_plugins/test_spf.py
+++ tests/functional/test_plugins/test_spf.py
@@ -249,12 +249,27 @@ Received-SPF: pass (example.com: domain of test@example.com) identity=helo"""
 
 	def test_spf_multiple_match(self):
 
-		email="""Received-SPF: fail (example.com: domain of test@example.com) 
-Received-SPF: pass (example.com: domain of test@example.com) identity=helo"""
+		email="""Received-SPF: pass (example.org: domain of test@example.org) identity=helo
+Received-SPF: none (example.org: domain of test@example.org) 
+Received-SPF: softfail (example.org: domain of test@example.org) 
+Received-SPF: fail (example.org: domain of test@example.org) identity=helo"""
 
 		self.setup_conf(config=CONFIG, pre_config=PRE_CONFIG)
 		result = self.check_pad(email)
-		self.check_report(result, 2, ['SPF_HELO_PASS', 'SPF_FAIL'])
+		self.check_report(result, 2, ['SPF_HELO_FAIL', 'SPF_SOFTFAIL'])
+
+	def test_spf_multiple_match_with_use_newest_received_spf_header_method(self):
+
+		lists"""use_newest_received_spf_header 1"""
+
+		email="""Received-SPF: pass (example.org: domain of test@example.org) identity=helo
+Received-SPF: none (example.org: domain of test@example.org) 
+Received-SPF: softfail (example.org: domain of test@example.org) 
+Received-SPF: fail (example.org: domain of test@example.org) identity=helo"""
+
+		self.setup_conf(config=CONFIG, pre_config=PRE_CONFIG)
+		result = self.check_pad(email)
+		self.check_report(result, 2, ['SPF_HELO_PASS', 'SPF_NONE'])
 
 	def test_spf_pass_other_identity(self):
 
@@ -282,6 +297,162 @@ Received-SPF: softfail (example.org: domain of test@example.org) identity=helo""
 		self.setup_conf(config=CONFIG, pre_config=PRE_CONFIG + lists)
 		result = self.check_pad(email)
 		self.check_report(result, 0, [])
+
+	def test_spf_with_ignore_received_spf_header_method_pass_helo_and_ident(self):
+
+		lists="""ignore_received_spf_header 1"""
+
+		email="""Received-SPF: fail (example.org: domain of test@example.org) identity=helo
+Received: from google.com ([2a00:1450:4017:804::200e]) by test.com
+	(envelope-from <test@google.com>)"""
+
+		self.setup_conf(config=CONFIG, pre_config=PRE_CONFIG + lists)
+		result = self.check_pad(email)
+		self.check_report(result, 2, ['SPF_PASS', 'SPF_HELO_PASS'])
+
+	def test_spf_with_no_receive_spf_header_pass_helo_and_ident(self):
+
+		email="""Received: from google.com ([2a00:1450:4017:804::200e]) by test.com
+	(envelope-from <test@google.com>)"""
+
+		self.setup_conf(config=CONFIG, pre_config=PRE_CONFIG)
+		result = self.check_pad(email)
+		self.check_report(result, 2, ['SPF_PASS', 'SPF_HELO_PASS'])
+
+	def test_spf_with_no_receive_spf_header_ident_not_matching_ip(self):
+
+		email="""Received: from google.com ([1.2.3.4]) by test.com
+	(envelope-from <test@google.com>)"""
+
+		self.setup_conf(config=CONFIG, pre_config=PRE_CONFIG)
+		result = self.check_pad(email)
+		self.check_report(result, 2, ['SPF_SOFTFAIL', 'SPF_HELO_SOFTFAIL'])	
+
+	def test_spf_with_no_receive_spf_header_ident_not_matching_ip_and_fail_on_helo(self):
+
+		email="""Received: from example.com ([1.2.3.4]) by test.com
+	(envelope-from <test@google.com>)"""
+
+		self.setup_conf(config=CONFIG, pre_config=PRE_CONFIG)
+		result = self.check_pad(email)
+		self.check_report(result, 2, ['SPF_SOFTFAIL', 'SPF_HELO_FAIL'])	
+
+	def test_spf_with_no_receive_spf_header_ident_matching_ip_and_fail_on_helo(self):
+
+		email="""Received: from example.com ([2a00:1450:4017:804::200e]) by test.com
+	(envelope-from <test@google.com>)"""
+
+		self.setup_conf(config=CONFIG, pre_config=PRE_CONFIG)
+		result = self.check_pad(email)
+		self.check_report(result, 2, ['SPF_PASS', 'SPF_HELO_FAIL'])	
+
+	def test_spf_with_no_receive_spf_header_invalid_ip(self):
+
+		lists="""ignore_received_spf_header 1"""
+
+		email="""Received: from google.com ([1.2.3333.4]) by test.com
+	(envelope-from <test@google.com>)"""
+
+		self.setup_conf(config=CONFIG, pre_config=PRE_CONFIG + lists)
+		result = self.check_pad(email)
+		self.check_report(result, 0, [])
+
+	def test_spf_with_no_receive_spf_header_invalid_helo(self):
+
+		email="""Received: from google ([2a00:1450:4017:803::200e]) by test.com
+	(envelope-from <test@google.com>)"""
+
+		self.setup_conf(config=CONFIG, pre_config=PRE_CONFIG)
+		result = self.check_pad(email)
+		self.check_report(result, 1, ['SPF_PASS'])
+
+	def test_spf_with_no_receive_spf_header_no_ident(self):
+
+		email="""Received: from google.com ([2a00:1450:4017:803::200e]) by test.com"""
+
+		self.setup_conf(config=CONFIG, pre_config=PRE_CONFIG)
+		result = self.check_pad(email)
+		self.check_report(result, 1, ['SPF_HELO_PASS'])
+
+	def test_spf_with_no_receive_spf_header_with_ignore_received_spf_method_no_ident(self):
+
+		lists="""ignore_received_spf_header 1"""
+
+		email="""Received: from google.com ([2a00:1450:4017:803::200e]) by test.com"""
+
+		self.setup_conf(config=CONFIG, pre_config=PRE_CONFIG + lists)
+		result = self.check_pad(email)
+		self.check_report(result, 1, ['SPF_HELO_PASS'])
+
+	def test_spf_with_no_receive_spf_header_no_ident_invalid_helo(self):
+
+		lists="""ignore_received_spf_header 1"""
+
+		email="""Received: from google ([2a00:1450:4017:803::200e]) by test.com"""
+
+		self.setup_conf(config=CONFIG, pre_config=PRE_CONFIG + lists)
+		result = self.check_pad(email)
+		self.check_report(result, 0, [])
+
+	def test_spf_with_no_receive_spf_header_no_ident_no_helo(self):
+
+		lists="""ignore_received_spf_header 1"""
+
+		email="""Received: from ([2a00:1450:4017:803::200e]) by test.com"""
+
+		self.setup_conf(config=CONFIG, pre_config=PRE_CONFIG + lists)
+		result = self.check_pad(email)
+		self.check_report(result, 0, [])
+
+	def test_spf_with_no_receive_spf_header_with_invalid_spf_timeout_method(self):
+		#should ignore spf_timeout and take default
+
+		lists="""ignore_received_spf_header 1
+				 spf_timeout a"""
+
+		email="""Received: from example.com ([2a00:1450:4017:804::200e]) by test.com
+	(envelope-from <test@google.com>)"""
+
+		self.setup_conf(config=CONFIG, pre_config=PRE_CONFIG + lists)
+		result = self.check_pad(email)
+		self.check_report(result, 2, ['SPF_PASS', 'SPF_HELO_FAIL'])
+
+	def test_spf_with_no_receive_spf_header_with_negative_spf_timeout_method(self):
+		#should ignore spf_timeout and take default
+
+		lists="""ignore_received_spf_header 1
+				 spf_timeout -2"""
+
+		email="""Received: from example.com ([2a00:1450:4017:804::200e]) by test.com
+	(envelope-from <test@google.com>)"""
+
+		self.setup_conf(config=CONFIG, pre_config=PRE_CONFIG + lists)
+		result = self.check_pad(email)
+		self.check_report(result, 2, ['SPF_PASS', 'SPF_HELO_FAIL'])
+
+	def test_spf_with_no_receive_spf_header_with_null_spf_timeout_method(self):
+
+		lists="""ignore_received_spf_header 1
+				 spf_timeout 0"""
+
+		email="""Received: from example.com ([2a00:1450:4017:804::200e]) by test.com
+	(envelope-from <test@google.com>)"""
+
+		self.setup_conf(config=CONFIG, pre_config=PRE_CONFIG + lists)
+		result = self.check_pad(email)
+		self.check_report(result, 0, [])
+
+	def test_spf_with_no_receive_spf_header_with_float_spf_timeout_method(self):
+
+		lists="""ignore_received_spf_header 1
+				 spf_timeout 0.5"""
+
+		email="""Received: from example.com ([2a00:1450:4017:804::200e]) by test.com
+	(envelope-from <test@google.com>)"""
+
+		self.setup_conf(config=CONFIG, pre_config=PRE_CONFIG + lists)
+		result = self.check_pad(email)
+		self.check_report(result, 2, ['SPF_PASS', 'SPF_HELO_FAIL'])
 
 def suite():
     """Gather all the tests from this package in a test suite."""
