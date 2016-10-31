4a9b7af6ea172c10d38ac36ef618da14f5fb0e30
Cosmin Grozea <cosmin.grozea94@gmail.com>
2016-10-31 17:06:06 +0200

    Refs #66 Add functional tests for unsafe report template

--- tests/functional/test_ruleset.py
+++ tests/functional/test_ruleset.py
@@ -21,9 +21,32 @@ report _REPORT_
 # Define rules for plugin
 CONFIG = """
 full   CHECK_TEST_EXIST  /test/
+score CHECK_TEST_EXIST 5
 describe CHECK_TEST_EXIST This description is in US english
 lang en_GB describe CHECK_TEST_EXIST This description is in GB english
 """
+
+UNSAFE_PRE_CONFIG = """
+report Normal report
+unsafe_report Unsafe report
+unsafe_report Unsafe report appended
+"""
+
+SPAM_EMAIL = """Content-Type: multipart/mixed; boundary=001a1140b054739bd2054029cd16
+
+Subject: Test Spam
+
+test
+
+--001a1140b054739bd2054029cd16
+Content-Type: application/octet-stream; name=keep
+Content-Disposition: attachment; filename=keep
+Content-Transfer-Encoding: base64
+X-Attachment-Id: f_iuy5ajvs0
+
+--001a1140b054739bd2054029cd16--
+"""
+
 
 class TestReport(tests.util.TestBase):
 
@@ -39,42 +62,53 @@ class TestReport(tests.util.TestBase):
         result = self.check_pad("Subject: test\n\nTest message.")
         self.assertEqual(result, "Test report template: No")
 
+
 class TestLocaleReport(tests.util.TestBase):
     """Class containing tests for rules locale description and report locale
     description"""
 
     def test_default_locale_rule_description(self):
-        """If locale is set to en_US the default rule description should be displayed"""
+        """If locale is set to en_US the default rule description
+        should be displayed"""
         my_env = os.environ.copy()
         my_env["LC_MESSAGES"] = "en_US.utf8"
         my_env["LC_ALL"] = ""
 
         self.setup_conf(pre_config=PRE_CONFIG, config=CONFIG)
-        result = self.check_pad(message="Subject: test\n\nTest message.", env=my_env)
-        self.assertEqual(result, "* 1.0 CHECK_TEST_EXIST This description is in US english")
+        result = self.check_pad(message="Subject: test\n\nTest message.",
+                                env=my_env)
+        self.assertEqual(result, "* 5.0 CHECK_TEST_EXIST "
+                                 "This description is in US english")
 
     def test_custom_locale_rule_description(self):
-        """If locale is set to en_GB the corresponding rule description should be displayed"""
+        """If locale is set to en_GB the corresponding rule description
+        should be displayed"""
         my_env = os.environ.copy()
         my_env["LC_MESSAGES"] = "en_GB.utf8"
         my_env["LC_ALL"] = ""
 
         self.setup_conf(pre_config=PRE_CONFIG, config=CONFIG)
-        result = self.check_pad(message="Subject: test\n\nTest message.", env=my_env)
-        self.assertEqual(result, "* 1.0 CHECK_TEST_EXIST This description is in GB english")
+        result = self.check_pad(message="Subject: test\n\nTest message.",
+                                env=my_env)
+        self.assertEqual(result, "* 5.0 CHECK_TEST_EXIST "
+                                 "This description is in GB english")
 
     def test_override_custom_locale_rule_description(self):
-        """If locale is set to en-GB and two descriptions are used for this language,
-        the las corresponding description should be displayed."""
+        """If locale is set to en-GB and two descriptions are used for this
+        language, the las corresponding description should be displayed."""
         my_env = os.environ.copy()
         my_env["LC_MESSAGES"] = "en_GB.utf8"
         my_env["LC_ALL"] = ""
 
-        second_description = "\nlang en_GB describe CHECK_TEST_EXIST Last description"
+        second_description = ("\nlang en_GB describe CHECK_TEST_EXIST "
+                              "Last description")
 
-        self.setup_conf(pre_config=PRE_CONFIG, config=CONFIG + second_description)
-        result = self.check_pad(message="Subject: test\n\nTest message.", env=my_env)
-        self.assertEqual(result, "* 1.0 CHECK_TEST_EXIST Last description")
+        self.setup_conf(pre_config=PRE_CONFIG,
+                        config=CONFIG + second_description)
+        result = self.check_pad(message="Subject: test\n\nTest message.",
+                                env=my_env)
+        self.assertEqual(result, "* 5.0 CHECK_TEST_EXIST "
+                                 "Last description")
 
     def test_default_locale_report_description(self):
         """If locale is set to en_US only the default report description
@@ -86,7 +120,8 @@ class TestLocaleReport(tests.util.TestBase):
         report_description = """\nreport This text is in US english
         \nlang en_GB report This text is in GB english"""
 
-        self.setup_conf(pre_config=PRE_CONFIG, config=CONFIG + report_description)
+        self.setup_conf(pre_config=PRE_CONFIG,
+                        config=CONFIG + report_description)
         result = self.check_pad(message="Empty message", env=my_env)
         self.assertEqual(result, "This text is in US english")
 
@@ -100,13 +135,15 @@ class TestLocaleReport(tests.util.TestBase):
         report_description = """\nreport This text is in US english
         \nlang en_GB report This text is in GB english"""
 
-        self.setup_conf(pre_config=PRE_CONFIG, config=CONFIG + report_description)
+        self.setup_conf(pre_config=PRE_CONFIG,
+                        config=CONFIG + report_description)
         result = self.check_pad(message="Empty message", env=my_env)
-        self.assertEqual(result, "This text is in US english\nThis text is in GB english")
+        self.assertEqual(result, "This text is in US english\n"
+                                 "This text is in GB english")
 
     def test_override_custom_locale_report_description(self):
-        """If locale is set to en_GB the default report description + all the en_GB
-        report descriptions should be displayed"""
+        """If locale is set to en_GB the default report description + all
+        the en_GB report descriptions should be displayed"""
         my_env = os.environ.copy()
         my_env["LC_MESSAGES"] = "en_GB.utf8"
         my_env["LC_ALL"] = ""
@@ -115,7 +152,41 @@ class TestLocaleReport(tests.util.TestBase):
         \nlang en_GB report This text is in GB english
         \nlang en_GB report This second text is in GB english"""
 
-        self.setup_conf(pre_config=PRE_CONFIG, config=CONFIG + report_description)
+        self.setup_conf(pre_config=PRE_CONFIG,
+                        config=CONFIG + report_description)
         result = self.check_pad(message="Empty message", env=my_env)
         self.assertEqual(result,
-            "This text is in US english\nThis text is in GB english\nThis second text is in GB english")
+                         "This text is in US english\n"
+                         "This text is in GB english\n"
+                         "This second text is in GB english")
+
+
+class TestUnsafeReportTemplate(tests.util.TestBase):
+
+    def test_unsfe_report_template_enabled(self):
+        self.setup_conf(pre_config=UNSAFE_PRE_CONFIG, config=CONFIG)
+        result = self.check_pad(message=SPAM_EMAIL,
+                                report_only=False, message_only=True)
+
+        self.assertTrue("Normal report" in result)
+        self.assertTrue("Unsafe report" in result)
+        self.assertTrue("Unsafe report appended" in result)
+
+    def test_unsfe_report_template_disabled(self):
+        self.setup_conf(pre_config=UNSAFE_PRE_CONFIG, config=CONFIG)
+        result = self.check_pad(message="Test email",
+                                report_only=False, message_only=True)
+
+        self.assertTrue("Normal report" not in result)
+        self.assertTrue("Unsafe report" not in result)
+        self.assertTrue("Unsafe report appended" not in result)
+
+    def test_clear_unsafe_report_template(self):
+        self.setup_conf(pre_config=UNSAFE_PRE_CONFIG +
+                        "\n clear_unsafe_report_template", config=CONFIG)
+        result = self.check_pad(message=SPAM_EMAIL, report_only=False,
+                                message_only=True)
+
+        self.assertTrue("Normal report" in result)
+        self.assertTrue("Unsafe report" not in result)
+        self.assertTrue("Unsafe report appended" not in result)
