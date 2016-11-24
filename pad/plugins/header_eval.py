fc6c22e1e60fc49de1a4777e6f8c9aee6f5d6b20
Alexandru Chirila <alex@alexkiro.com>
2016-11-24 15:30:09 +0200

    Refs. #28. Add base class for the new header_eval plugin.

---
+++ pad/plugins/header_eval.py
@@ -0,0 +1,145 @@
+"""Expose some eval rules that do checks on the headers."""
+
+from __future__ import division
+from __future__ import absolute_import
+
+import re
+import collections
+
+import pad.plugins.base
+
+from pad.regex import Regex
+
+
+class HeaderEval(pad.plugins.base.BasePlugin):
+    eval_rules = (
+        "check_for_fake_aol_relay_in_rcvd",
+        "check_for_faraway_charset_in_headers",
+        "check_for_unique_subject_id",
+        "check_illegal_chars",
+        "check_for_forged_hotmail_received_headers",
+        "check_for_no_hotmail_received_headers",
+        "check_for_msn_groups_headers",
+        "check_for_forged_eudoramail_received_headers",
+        "check_for_forged_yahoo_received_headers",
+        "check_for_forged_juno_received_headers",
+        "check_for_matching_env_and_hdr_from",
+        "sorted_recipients",
+        "similar_recipients",
+        "check_for_missing_to_header",
+        "check_for_forged_gw05_received_headers",
+        "check_for_round_the_world_received_helo",
+        "check_for_round_the_world_received_revdns",
+        "check_for_shifted_date",
+        "subject_is_all_caps",
+        "check_for_to_in_subject",
+        "check_outlook_message_id",
+        "check_messageid_not_usable",
+        "check_header_count_range",
+        "check_unresolved_template",
+        "check_ratware_name_id",
+        "check_ratware_envelope_from",
+    )
+
+    def check_for_fake_aol_relay_in_rcvd(self, msg, target=None):
+        return False
+
+
+    def check_for_faraway_charset_in_headers(self, msg, target=None):
+        return False
+
+
+    def check_for_unique_subject_id(self, msg, target=None):
+        return False
+
+
+    def check_illegal_chars(self, msg, target=None):
+        return False
+
+
+    def check_for_forged_hotmail_received_headers(self, msg, target=None):
+        return False
+
+
+    def check_for_no_hotmail_received_headers(self, msg, target=None):
+        return False
+
+
+    def check_for_msn_groups_headers(self, msg, target=None):
+        return False
+
+
+    def check_for_forged_eudoramail_received_headers(self, msg, target=None):
+        return False
+
+
+    def check_for_forged_yahoo_received_headers(self, msg, target=None):
+        return False
+
+
+    def check_for_forged_juno_received_headers(self, msg, target=None):
+        return False
+
+
+    def check_for_matching_env_and_hdr_from(self, msg, target=None):
+        return False
+
+
+    def sorted_recipients(self, msg, target=None):
+        return False
+
+
+    def similar_recipients(self, msg, target=None):
+        return False
+
+
+    def check_for_missing_to_header(self, msg, target=None):
+        return False
+
+
+    def check_for_forged_gw05_received_headers(self, msg, target=None):
+        return False
+
+
+    def check_for_round_the_world_received_helo(self, msg, target=None):
+        return False
+
+
+    def check_for_round_the_world_received_revdns(self, msg, target=None):
+        return False
+
+
+    def check_for_shifted_date(self, msg, target=None):
+        return False
+
+
+    def subject_is_all_caps(self, msg, target=None):
+        return False
+
+
+    def check_for_to_in_subject(self, msg, target=None):
+        return False
+
+
+    def check_outlook_message_id(self, msg, target=None):
+        return False
+
+
+    def check_messageid_not_usable(self, msg, target=None):
+        return False
+
+
+    def check_header_count_range(self, msg, target=None):
+        return False
+
+
+    def check_unresolved_template(self, msg, target=None):
+        return False
+
+
+    def check_ratware_name_id(self, msg, target=None):
+        return False
+
+
+    def check_ratware_envelope_from(self, msg, target=None):
+        return False
