6721021d456764c6e1cb9b1828cc63edbe391065
Catalina Ghiorghita <cata.ghiorghita@gmail.com>
2016-12-13 11:24:46 +0200

    Refs #38 Add more unittests for check_dkim_signature method in order to cover except cases

--- tests/unit/test_plugins/test_dkim.py
+++ tests/unit/test_plugins/test_dkim.py
@@ -1,10 +1,11 @@
 import unittest
-from collections import defaultdict
 
 try:
     from unittest.mock import patch, Mock, MagicMock, call
 except ImportError:
     from mock import patch, Mock, MagicMock, call
+
+import dkim
 
 import pad.plugins.dkim
 
@@ -70,6 +71,15 @@ class TestGetHeader(unittest.TestCase):
         result_expected = {b".*@gmail.com": "gmail.com"}
         self.assertEqual(result, result_expected)
 
+    def test_parse_input_unwhitelist(self):
+        list_name = "whitelist_from_dkim"
+        self.global_data["whitelist_from_dkim"] = [
+            "user@example.com"]
+        self.global_data["unwhitelist_from_dkim"] = ["user@example.com"]
+        result = self.plug.parse_input(list_name)
+        result_expected = {}
+        self.assertEqual(result, result_expected)
+
     def test_parse_input_no_domain(self):
         list_name = "def_whitelist_from_dkim"
         self.global_data["def_whitelist_from_dkim"] = [
@@ -77,6 +87,45 @@ class TestGetHeader(unittest.TestCase):
         result = self.plug.parse_input(list_name)
         result_expected = {b".*@gmail.com": ""}
         self.assertEqual(result, result_expected)
+
+
+class TestGetTxt(unittest.TestCase):
+    def setUp(self):
+        unittest.TestCase.setUp(self)
+        self.options = {}
+        self.global_data = {}
+        self.msg_data = {}
+
+        self.mock_ctxt = MagicMock(**{
+            "get_plugin_data.side_effect": lambda p, k: self.global_data[k],
+            "set_plugin_data.side_effect": lambda p, k, v: self.global_data.setdefault(k, v)}
+        )
+        self.mock_msg = MagicMock(**{
+            "get_plugin_data.side_effect": lambda p, k: self.msg_data[k],
+            "set_plugin_data.side_effect": lambda p, k, v: self.msg_data.setdefault(k, v),
+        })
+
+        self.mock_get_txt_dnspython = patch(
+            "pad.plugins.dkim.DKIMPlugin.get_txt_dnspython").start()
+
+        self.plug = pad.plugins.dkim.DKIMPlugin(self.mock_ctxt)
+
+    def tearDown(self):
+        unittest.TestCase.tearDown(self)
+        patch.stopall()
+
+    def test_get_txt(self):
+        txt = 'k=rsa; p=MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEA1' \
+              'Kd87/UeJjenpabgbFwh+eBCsSTrqmwIYYvywlbhbqoo2DymndFkbjO' \
+              'VIPIldNs/m40KF+yzMn1skyoxcTUGCQs8g3FgD2Ap3ZB5DekAo5wMm' \
+              'k4wimDO+U8QzI3SD07y2+07wlNWwIt8svnxgdxGkVbbhzY8i+RQ9Dp' \
+              'SVpPbF7ykQxtKXkv/ahW3KjViiAH+ghvvIhkx4xYSIc9oSwVmAl5Oc' \
+              'tMEeWUwg8Istjqz8BZeTWbf41fbNhte7Y+YqZOwq1Sd0DbvYAD9NOZ' \
+              'K9vlfuac0598HY+vtSBczUiKERHv1yRbcaQtZFh5wtiRrN04BLUTD21' \
+              'MycBX5jYchHjPY/wIDAQAB'
+        self.mock_get_txt_dnspython.return_value = txt
+        result = self.plug.get_txt(b'20120113._domainkey.gmail.com.')
+        self.assertEqual(result, txt.encode('utf-8'))
 
 
 class TestGetAuthors(unittest.TestCase):
@@ -475,6 +524,88 @@ class TestCheckSignature(unittest.TestCase):
 
         self.assertEqual((self.plug.dkim_valid, self.plug.dkim_signed,
                           self.plug.dkim_has_valid_author_sig), (1, 1, 1))
+
+    def test_check_dkim_signature_message_format_error(self):
+        message = "Message"
+        dkim_signature = "Signature"
+        parsed_signature = {b'a': b'rsa-sha256',
+                            b'c': b'relaxed/relaxed',
+                            b's': b'20120113',
+                            b'h': b'mime-version:from:date:message-id:subject:to',
+                            b'v': b'1',
+                            b'bh': b'Dv0CkJSC1ekvQd/JcC/hXp/1AjTsoZ+GIbd7HbXO/4M=',
+                            b'd': b'gmail.com',
+                            b'b': b'fgMfuR+WI3pI+yUJnZgwdOs1BU5DEepzyF4Z0QNvTvnGj79OVjCcHY70JrDglExU+c\n         '
+                                  b'mCQsc3yocY/k5jMeU47imXjWFxeJGdHSlNe6mQf5GsXMizEl97BbcDkkGso66RjrJ4dE\n         '
+                                  b'2Na4bQ/rJGU4gTuhD3bMEvkfXXWNHbDyCPjZnOAz97qryWHZZauKzqJ9pRgpj9cj9Rge\n         '
+                                  b'TMoOYbv/exIF/VIiC9IXiCFmFY0NVCbqi1ksbjt/0cp+S1NeEl95d2FkAkOUPsCu9kto\n         '
+                                  b'eriiEP6KqssKrmmX4XC2ovcTg9fxJZeS2VsgBOT0WHXDMEtp1KldthDIOZYVMZFbXRlc\n         '
+                                  b'RsvA=='}
+        self.global_data["dkim_minimum_key_bits"] = -2
+        self.mock_msg.raw_msg = message
+        self.mock_msg.get.return_value = dkim_signature
+        self.mock_dkim_parse_tag.return_value = parsed_signature
+        self.mock_dkim_verify.side_effect = dkim.MessageFormatError
+        self.plug.author_domains = [b"gmail.com"]
+        self.plug.check_dkim_signature(self.mock_msg)
+
+        self.assertEqual((self.plug.dkim_valid, self.plug.dkim_signed,
+                          self.plug.dkim_has_valid_author_sig), (0, 1, 0))
+
+    def test_check_dkim_signature_message_validation_error(self):
+        message = "Message"
+        dkim_signature = "Signature"
+        parsed_signature = {b'a': b'rsa-sha256',
+                            b'c': b'relaxed/relaxed',
+                            b's': b'20120113',
+                            b'h': b'mime-version:from:date:message-id:subject:to',
+                            b'v': b'1',
+                            b'bh': b'Dv0CkJSC1ekvQd/JcC/hXp/1AjTsoZ+GIbd7HbXO/4M=',
+                            b'd': b'gmail.com',
+                            b'b': b'fgMfuR+WI3pI+yUJnZgwdOs1BU5DEepzyF4Z0QNvTvnGj79OVjCcHY70JrDglExU+c\n         '
+                                  b'mCQsc3yocY/k5jMeU47imXjWFxeJGdHSlNe6mQf5GsXMizEl97BbcDkkGso66RjrJ4dE\n         '
+                                  b'2Na4bQ/rJGU4gTuhD3bMEvkfXXWNHbDyCPjZnOAz97qryWHZZauKzqJ9pRgpj9cj9Rge\n         '
+                                  b'TMoOYbv/exIF/VIiC9IXiCFmFY0NVCbqi1ksbjt/0cp+S1NeEl95d2FkAkOUPsCu9kto\n         '
+                                  b'eriiEP6KqssKrmmX4XC2ovcTg9fxJZeS2VsgBOT0WHXDMEtp1KldthDIOZYVMZFbXRlc\n         '
+                                  b'RsvA=='}
+        self.global_data["dkim_minimum_key_bits"] = -2
+        self.mock_msg.raw_msg = message
+        self.mock_msg.get.return_value = dkim_signature
+        self.mock_dkim_parse_tag.return_value = parsed_signature
+        self.mock_dkim_verify.side_effect = dkim.ValidationError
+        self.plug.author_domains = [b"gmail.com"]
+        self.plug.check_dkim_signature(self.mock_msg)
+
+        self.assertEqual((self.plug.dkim_valid, self.plug.dkim_signed,
+                          self.plug.dkim_has_valid_author_sig), (0, 1, 0))
+
+    def test_check_dkim_signature_message_key_format_error(self):
+        message = "Message"
+        dkim_signature = "Signature"
+        parsed_signature = {b'a': b'rsa-sha256',
+                            b'c': b'relaxed/relaxed',
+                            b's': b'20120113',
+                            b'h': b'mime-version:from:date:message-id:subject:to',
+                            b'v': b'1',
+                            b'bh': b'Dv0CkJSC1ekvQd/JcC/hXp/1AjTsoZ+GIbd7HbXO/4M=',
+                            b'd': b'gmail.com',
+                            b'b': b'fgMfuR+WI3pI+yUJnZgwdOs1BU5DEepzyF4Z0QNvTvnGj79OVjCcHY70JrDglExU+c\n         '
+                                  b'mCQsc3yocY/k5jMeU47imXjWFxeJGdHSlNe6mQf5GsXMizEl97BbcDkkGso66RjrJ4dE\n         '
+                                  b'2Na4bQ/rJGU4gTuhD3bMEvkfXXWNHbDyCPjZnOAz97qryWHZZauKzqJ9pRgpj9cj9Rge\n         '
+                                  b'TMoOYbv/exIF/VIiC9IXiCFmFY0NVCbqi1ksbjt/0cp+S1NeEl95d2FkAkOUPsCu9kto\n         '
+                                  b'eriiEP6KqssKrmmX4XC2ovcTg9fxJZeS2VsgBOT0WHXDMEtp1KldthDIOZYVMZFbXRlc\n         '
+                                  b'RsvA=='}
+        self.global_data["dkim_minimum_key_bits"] = -2
+        self.mock_msg.raw_msg = message
+        self.mock_msg.get.return_value = dkim_signature
+        self.mock_dkim_parse_tag.return_value = parsed_signature
+        self.mock_dkim_verify.side_effect = dkim.KeyFormatError
+        self.plug.author_domains = [b"gmail.com"]
+        self.plug.check_dkim_signature(self.mock_msg)
+
+        self.assertEqual((self.plug.dkim_valid, self.plug.dkim_signed,
+                          self.plug.dkim_has_valid_author_sig), (0, 1, 0))
+
 
     def test_check_dkim_signature_uncorrect_signature_domain(self):
         message = "Message"
