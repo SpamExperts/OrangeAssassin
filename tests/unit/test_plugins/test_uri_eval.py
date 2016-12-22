ceb822224e754adeea136125de039482c70b3a42
Roxana Butuc <roxana@spamexperts.com>
2016-12-22 14:45:30 +0200

    Refs.#31 Modify unit tests for uri_eval plugin.

--- tests/unit/test_plugins/test_uri_eval.py
+++ tests/unit/test_plugins/test_uri_eval.py
@@ -1,4 +1,5 @@
 """Tests for pad.plugins.uri_eval plugin"""
+import random
 import unittest
 
 try:
@@ -11,19 +12,15 @@ import pad.context
 import pad.message
 import pad.plugins.uri_eval
 
+ascii_lowercase = 'abcdefghijklmnopqrstuvwxyz'
+ascii_uppercase = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+ascii_letters = ascii_lowercase + ascii_uppercase
 
 class TestURIDetail(unittest.TestCase):
     """Tests for the URIDetail plugin"""
     def setUp(self):
         unittest.TestCase.setUp(self)
         self.options = {}
-        # self.global_data = {"geodb":"/innexistent/location/"}
-        # self.cmds = {"uri_detail": pad.plugins.uri_detail.URIDetailRule}
-        # patch("pad.plugins.uri_detail.URIDetailPlugin.options",
-        #       self.options).start()
-        # patch("pad.plugins.uri_detail.URIDetailPlugin.cmds",
-        #       self.cmds).start()
-
         self.mock_ctxt = MagicMock(**{
             "get_plugin_data.side_effect": lambda p, k: self.global_data[k],
             "set_plugin_data.side_effect": lambda p, k,
@@ -62,12 +59,13 @@ class TestURIDetail(unittest.TestCase):
             ["https://www.paypal.com/login/account-unlock"]
         self.mock_msg.uri_detail_links = \
             {'http://45.42.12.12/login/account-unlock':
-                 {'cleaned': 'http://45.42.12.12/login/account-unlock',
-                  'domain': '45.42.12.12',
-                  'raw': 'http://45.42.12.12/login/account-unlock',
-                  'scheme': 'http',
-                  'text': 'https://www.paypal.com/login/account-unlock',
-                  'type': 'a'}}
+                 {"a":
+                      {'cleaned': 'http://45.42.12.12/login/account-unlock',
+                       'domain': '45.42.12.12',
+                       'raw': 'http://45.42.12.12/login/account-unlock',
+                       'scheme': 'http',
+                       'text': 'https://www.paypal.com/login/account-unlock',}}
+                 }
         result = self.plugin.check_https_ip_mismatch(self.mock_msg)
         self.assertEqual(result, 1)
 
@@ -76,15 +74,15 @@ class TestURIDetail(unittest.TestCase):
             patch("pad.plugins.uri_eval.URIEvalPlugin."
                   "extract_url_from_text").start()
         self.mock_extract_url_from_text.return_value = \
-            ["http://www.paypal.com/login/account-unlock"]
+            ["http://www.paypal.com/login/account"]
         self.mock_msg.uri_detail_links = \
-            {'http://45.42.12.12/login/account-unlock':
-                 {'cleaned': 'http://45.42.12.12/login/account-unlock',
-                  'domain': '45.42.12.12',
-                  'raw': 'http://45.42.12.12/login/account-unlock',
-                  'scheme': 'http',
-                  'text': 'https://www.paypal.com/login/account-unlock',
-                  'type': 'a'}}
+            {'http://45.42.12.12/login/account':
+                 {"a":
+                      {'cleaned': 'http://45.42.12.12/login/account',
+                       'domain': '45.42.12.12',
+                       'raw': 'http://45.42.12.12/login/account',
+                       'scheme': 'http',
+                       'text': 'https://www.paypal.com/login/account', }}}
         result = self.plugin.check_https_ip_mismatch(self.mock_msg)
         self.assertEqual(result, 0)
 
@@ -93,15 +91,15 @@ class TestURIDetail(unittest.TestCase):
             patch("pad.plugins.uri_eval.URIEvalPlugin."
                   "extract_url_from_text").start()
         self.mock_extract_url_from_text.return_value = \
-            ["http://www.paypal.com/login/account-unlock"]
+            ["http://www.paypal.com/login/account"]
         self.mock_msg.uri_detail_links = \
-            {'http://example.com/login/account-unlock':
-                 {'cleaned': 'http://example.com/login/account-unlock',
-                  'domain': 'example.com',
-                  'raw': 'http://example.com/login/account-unlock',
-                  'scheme': 'http',
-                  'text': 'https://www.paypal.com/login/account-unlock',
-                  'type': 'a'}}
+            {'http://example.com/login/account':
+                 {"a":
+                      {'cleaned': 'http://example.com/login/account',
+                       'domain': 'example.com',
+                       'raw': 'http://example.com/login/account',
+                       'scheme': 'http',
+                       'text': 'https://www.paypal.com/login/account',}}}
         result = self.plugin.check_https_ip_mismatch(self.mock_msg)
         self.assertEqual(result, 0)
 
@@ -116,55 +114,10 @@ class TestURIDetail(unittest.TestCase):
         self.assertEqual(result, 0)
 
     def test_extract_url_from_text(self):
-        text = "Anchor text with https://example.com"
+        text = ["Anchor text with https://example.com"]
         result = self.plugin.extract_url_from_text(text)
         self.assertEqual(result, ["https://example.com"])
 
-    def test_match_uri_truncated(self):
-        key = 'https://www.PAYPAL.com/login/account-unlock'
-        uri_list = ['https://www.PAYPAL.com/...']
-        self.mock_urlparse = patch("pad.plugins.uri_eval.urlparse").start()
-        self.mock_urlparse.return_value.netloc =  'www.PAYPAL.com'
-
-        uri_details = \
-                 {'cleaned': 'https://www.PAYPAL.com/...',
-                  'domain': 'www.PAYPAL.com',
-                  'raw': 'https://www.PAYPAL.com/...',
-                  'scheme': 'https',
-                  'type': 'parsed'}
-        result = self.plugin.match_uri_truncated(key, uri_list, uri_details)
-        self.assertEqual(result, True)
-
-    def test_match_uri_truncated_False(self):
-        key = 'https://www.PAYPAL.com/login/account-unlock'
-        uri_list = ['https://www.PAYPAL.com/login/account-unlock']
-        self.mock_urlparse = patch("pad.plugins.uri_eval.urlparse").start()
-        self.mock_urlparse.return_value.netloc = 'www.PAYPAL.com'
-        uri_details = \
-                 {'cleaned': 'https://www.PAYPAL.com/login/account-unlock',
-                  'domain': 'www.PAYPAL.com'}
-        result = self.plugin.match_uri_truncated(key, uri_list, uri_details)
-        self.assertEqual(result, False)
-
-    def test_check_uri_truncated(self):
-        self.mock_extract_url_from_text = \
-            patch("pad.plugins.uri_eval.URIEvalPlugin."
-                  "extract_url_from_text").start()
-        self.mock_extract_url_from_text.return_value = \
-            ["https://www.PAYPAL.com/..."]
-        self.mock_match_uri_truncated = \
-            patch("pad.plugins.uri_eval.URIEvalPlugin."
-                  "match_uri_truncated").start().return_value = True
-        self.mock_msg.uri_detail_links = \
-            {'https://www.PAYPAL.com/login/account-unlock':
-                 {'cleaned': 'https://www.PAYPAL.com/login/account-unlock',
-                  'domain': 'www.PAYPAL.com',
-                  'raw': 'https://www.PAYPAL.com/login/account-unlock',
-                  'scheme': 'https',
-                  'text': 'It is a truncated uri https://www.PAYPAL.com/...',
-                  'type': 'a'}}
-        result = self.plugin.check_uri_truncated(self.mock_msg)
-        self.assertEqual(result, 1)
 
     def test_check_uri_truncated_does_not_match(self):
         self.mock_extract_url_from_text = \
@@ -172,30 +125,29 @@ class TestURIDetail(unittest.TestCase):
                   "extract_url_from_text").start()
         self.mock_extract_url_from_text.return_value = \
             ["https://www.PAYPAL.com/..."]
-        self.mock_match_uri_truncated = \
-            patch("pad.plugins.uri_eval.URIEvalPlugin."
-                  "match_uri_truncated").start().return_value = False
-        # self.mock_msg.side_effect = KeyError
         self.mock_msg.uri_detail_links = \
             {'https://www.PAYPAL.com/login/account-unlock':
                  {'cleaned': 'https://www.PAYPAL.com/login/account-unlock',
                   'domain': 'www.PAYPAL.com',
                   'raw': 'https://www.PAYPAL.com/login/account-unlock',
                   'scheme': 'https',
-                  'text': 'It is a truncated uri https://www.PAYPAL.com/...',
-                  'type': 'a'}}
+                  'text': 'It is a truncated uri https://www.PAYPAL.com/...',}}
         result = self.plugin.check_uri_truncated(self.mock_msg)
         self.assertEqual(result, 0)
 
-    def test_check_uri_truncated_KeyError(self):
+    def test_check_uri_truncated(self):
+        mytext = [random.choice(ascii_letters) for _ in range(8192)]
+        long_text = "".join(mytext)
         self.mock_extract_url_from_text = \
             patch("pad.plugins.uri_eval.URIEvalPlugin."
                   "extract_url_from_text").start()
         self.mock_extract_url_from_text.return_value = \
             ["https://www.PAYPAL.com/..."]
-        self.mock_match_uri_truncated = \
-            patch("pad.plugins.uri_eval.URIEvalPlugin."
-                  "match_uri_truncated").start().return_value = False
-        self.mock_msg.side_effect = KeyError
+        self.mock_msg.uri_detail_links = \
+            {long_text:
+                 {"a":
+                      {'cleaned': 'https://www.PAYPAL.com/login/account',
+                       'domain': 'www.PAYPAL.com',
+                       'text': 'It is a truncated uri'}}}
         result = self.plugin.check_uri_truncated(self.mock_msg)
         self.assertEqual(result, 0)
