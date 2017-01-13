1f13db871356e40dae8fa6f20c329b8f3b6ab578
requires.io <support@requires.io>
2017-01-13 14:20:44 +1300

    [requires.io] dependency update

--- setup.py
+++ setup.py
@@ -2,8 +2,23 @@
 
 from __future__ import absolute_import
 
+import sys
+
 import pad
 import distutils.core
+
+# TODO: adjust this for CPython/PyPy
+with open("requirements/base.txt") as base:
+    requirements = base.readlines()
+if sys.version_info.major == 3:
+    with open("requirements/python3.txt") as py3:
+        requirements.extend(py3.readlines())
+elif sys.version_info.major == 2:
+    with open("requirements/python2.txt") as py2:
+        requirements.extend(py2.readlines())
+
+with open("requirements/tests.txt") as test:
+    test_requirements = test.readlines()
 
 distutils.core.setup(
     name='SpamPAD',
@@ -19,4 +34,6 @@ distutils.core.setup(
         'pad.plugins',
         'pad.protocol',
     ],
+    install_requires=requirements,
+    tests_require=test_requirements,
 )
